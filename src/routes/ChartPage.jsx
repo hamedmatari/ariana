5dab8c6b67b808a5f5a3dca61a86ddb636cd255e
hamedMatari <matari.hamed@yahoo.com>
2022-03-02 12:42:25 +0330

    Edit Chart page

--- src/routes/ChartPage.jsx
+++ src/routes/ChartPage.jsx
@@ -5,13 +5,17 @@ import { Chart, ChartJS } from "chart.js/auto";
 import { Pie } from "react-chartjs-2";
 import { Bar } from "react-chartjs-2";
 import { useSelector } from "react-redux";
-import { Box, Container } from "@mui/material";
+import { Card, Grid } from "@mui/material";
 
 export default function ChartPage() {
   document.title = "Charts | Ariana";
   const users = useSelector((state) => state.users.users);
 
+  const ages = users.map((user) => user.age);
+  const names = users.map((user) => user.name);
+
   const skills = users.map((user) => user.skills);
+
   const allSkills = {
     HTML: 0,
     React: 0,
@@ -24,15 +28,46 @@ export default function ChartPage() {
       allSkills[item]++;
     });
   });
-  const labels = Object.keys(allSkills);
+  const labelsPie = Object.keys(allSkills);
+  const valuesPie = Object.values(allSkills);
 
-  const values = Object.values(allSkills);
-
-  const data = {
-    labels,
+  const dataBar = {
+    labels: names,
     datasets: [
       {
-        data: values,
+        data: ages,
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.3)",
+          "rgba(25,  77, 0  , 0.3)",
+          "rgba(255, 206, 86, 0.3)",
+          "rgba(75, 192, 192, 0.3)",
+          "rgba(153, 102, 255, 0.3)",
+          "rgba(255, 159, 64, 0.3)",
+          "rgba(234, 99, 132, 0.3)",
+          "rgba(76, 162, 235, 0.3)",
+          "rgba(34, 206, 86, 0.4)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(65, 54, 255, 0.2)",
+          "rgba(123, 89, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const dataPie = {
+    labels: labelsPie,
+    datasets: [
+      {
+        data: valuesPie,
         backgroundColor: [
           "rgba(255, 99, 132, 0.3)",
           "rgba(25,  77, 0  , 0.3)",
@@ -61,8 +96,24 @@ export default function ChartPage() {
   };
 
   return (
-    <div style={{ width: "500px", margin: "50px auto" }}>
-      <Pie data={data} />
-    </div>
+    <Grid
+      container
+      direction="row"
+      justifyContent="center"
+      spacing={8}
+      alignItems="flex-end"
+      sx={{ p: 8 }}
+    >
+      <Grid item sx={{ width: "600px" }}>
+        <Card sx={{ p: 2 }}>
+          <Pie data={dataPie} />
+        </Card>
+      </Grid>
+      <Grid item sx={{ width: "700px" }}>
+        <Card sx={{ p: 2 }}>
+          <Bar data={dataBar} />
+        </Card>
+      </Grid>
+    </Grid>
   );
 }
