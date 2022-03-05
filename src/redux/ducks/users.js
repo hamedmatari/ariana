860c2b59ea606cf0b4ca90c01cf3354a8590bb14
hamedMatari <matari.hamed@yahoo.com>
2022-03-05 17:29:51 +0330

    Add ID to store / Edit reducer

--- src/redux/ducks/users.js
+++ src/redux/ducks/users.js
@@ -1,19 +1,48 @@
 const ADD_USER = "add-user";
+const DELETE_USER = "delete-user";
+const EDIT_USER = "edit-user";
 
 export const addUser = (user) => ({
   type: ADD_USER,
   payload: user,
 });
-
+export const deleteUser = (user) => ({
+  type: DELETE_USER,
+  payload: user,
+});
+export const editUser = (newUser, user) => ({
+  type: EDIT_USER,
+  newUser,
+  oldUser: user,
+});
 const initialState = {
   users: [],
+  lastID: 0,
 };
-
-export default (state = initialState, action) => {
+const reducerSwitch = (state = initialState, action) => {
   switch (action.type) {
     case ADD_USER:
+      action.payload.id = state.lastID;
+      state.lastID++;
       return { ...state, users: [...state.users, action.payload] };
+    case DELETE_USER:
+      return {
+        ...state,
+        users: state.users.filter((user) => user.id !== action.payload.id),
+      };
+    case EDIT_USER:
+      return {
+        ...state,
+        users: state.users.map((user) => {
+          if (user.id === action.oldUser.id) {
+            action.newUser.id = user.id;
+            return action.newUser;
+          }
+          return user;
+        }),
+      };
     default:
       return state;
   }
 };
+export default reducerSwitch;
